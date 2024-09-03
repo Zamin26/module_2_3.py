8da3e0d4cc56481a97272f9eaa937cad7dc72699
Zamin26 <zamin26@bk.ru>
2024-09-04 00:44:54 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -5,7 +5,6 @@ number = 0
 while len(my_list) > number:
     if my_list[number] > 0:
         print(my_list[number])
-    else:
-        if my_list[number] < 0:
-            break
+    if my_list[number] < 0:
+        break
     number = number + 1
